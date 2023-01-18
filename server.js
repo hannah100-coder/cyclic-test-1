d4b7247ed809316441c686c7ba0eb10f12092b0d
hannah100-coder <pd206@naver.com>
2023-01-18 00:40:06 -0500

    [chore]change output_name&studentnumber

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hannah Baek - 153755228");
 });
 
 // setup http server to listen on HTTP_PORT
